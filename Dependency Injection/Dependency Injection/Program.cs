18d2e88e546d2e23dd34f420c4608bdabd3ca089
devenderpra1 <58927567+devenderpra1@users.noreply.github.com>
2025-08-20 12:59:01 +0530

    Add files via upload

---
+++ Dependency Injection/Dependency Injection/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Dependency_Injection;
+using Dependency_InjectionTests;
+
+Console.WriteLine("Hello, World!");
+//var tokenProvider = new TokenProvider();
+//var token = tokenProvider.CreateToken();
+//token.LoginID.ToString();
+//tokenProvider.IsValidToken(token);
+
+var tokenTest = new TokenTests();
+tokenTest.SetupSerialization();
+Console.WriteLine(tokenTest.IsTokenValidAtCreation() ? "Yes" : "No");
+Console.WriteLine(tokenTest.IsTokenValidAfterTwoMinutes() ? "No" : "Yes");
---
+++ Design Pattern Factory and Abstract/Design Pattern Factory and Abstract/DBFactoryCreator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Design_Pattern_Factory_and_Abstract
+{
+    public class DBFactoryCreator
+    {
+        public IDatabaseFactory CreateFactory(string configuration)
+        {
+            if (configuration == "SQLServer")
+            {
+                return new SQLServerFactory();
+            }
+            else
+            {
+                return new MySQlServerFactory();
+            }
+        }
+    }
+}
