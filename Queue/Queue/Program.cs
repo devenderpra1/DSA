41672fdb0296575c342a8214f2c434e2b842985f
Devender <Devenderprasad@quantumphinance.com>
2026-05-07 19:47:29 +0530

    Master: Updated DSA

---
+++ Queue/Queue/Program.cs
@@ -0,0 +1,142 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public class QueueUsage
+{
+    public class MaxInfo
+    {
+        public int Value;
+
+        public int start;
+
+        public int end;
+    }
+
+    public List<MaxInfo> GetMaximumInEachWindow(List<int> inputs, int windowSize)
+    {
+        var infos = new List<MaxInfo>();
+        //First Window
+
+        var queueForProbableMax = new Queue<int>();
+        for (int i = 0; i < windowSize; i++)
+        {
+            while (queueForProbableMax.Count > 1 && queueForProbableMax.Peek() < inputs[i])
+            {
+                queueForProbableMax.Dequeue();
+            }
+            queueForProbableMax.Enqueue(inputs[i]);
+        }
+
+        infos.Add(new MaxInfo() { start = 0, end = windowSize - 1, Value = queueForProbableMax.Peek() });
+
+        for (int i = windowSize; i < inputs.Count; i++)
+        {
+            if (queueForProbableMax.Peek() == inputs[i - windowSize])
+            {
+                queueForProbableMax.Dequeue();
+            }
+            while (queueForProbableMax.Count > 1 && queueForProbableMax.Peek() < inputs[i])
+            {
+                queueForProbableMax.Dequeue();
+            }
+            queueForProbableMax.Enqueue(inputs[i]);
+            infos.Add(new MaxInfo() { start = i - windowSize + 1, end = windowSize, Value = queueForProbableMax.Peek() });
+        }
+        return infos;
+    }
+
+
+    public class DoubleEndedQueue<T>
+    {
+        public DoubleLinkedNode<T> front;
+
+        public DoubleLinkedNode<T> rear;
+
+        public T PeekFront()
+        {
+            if (front == null)
+                return default;
+            return front.Value;
+        }
+        public T PeekRear()
+        {
+            if (rear == null)
+                return default;
+            return rear.Value;
+        }
+        public void Enque(T value)
+        {
+            var node = new DoubleLinkedNode<T>(value);
+            node.NextBackward = rear;
+            if (rear != null)
+            {
+                rear.NextForward = node;
+            }
+            else//Single Node Condition
+            {
+                front = node;
+            }
+            rear = node;
+        }
+        public void EnqueFront(T value)
+        {
+            var node = new DoubleLinkedNode<T>(value);
+            node.NextForward = front;
+            if (front != null)
+            {
+                front.NextBackward = node;
+            }
+            else
+            {
+                rear = node;
+            }
+            front = node;
+        }
+
+        public T Deque()
+        {
+            if (front == null)
+                return default;
+            var removedNode = front;
+            front = front.NextForward;
+            if (front == null)
+            {
+                rear = null;
+            }
+            else
+                front.NextBackward = null;
+            return removedNode.Value;
+        }
+        public T DequeRear()
+        {
+            if (rear == null)
+                return default;
+            var removedNode = rear;
+            rear = rear.NextBackward;
+            if (rear == null)
+            {
+                front = null;
+            }
+            else
+            {
+                rear.NextForward = null;
+            }
+            return removedNode.Value;
+        }
+
+        public class DoubleLinkedNode<T>
+        {
+            public DoubleLinkedNode(T value)
+            {
+                this.Value = value;
+            }
+            public T Value;
+
+            public DoubleLinkedNode<T> NextBackward;
+
+            public DoubleLinkedNode<T> NextForward;
+        }
+    }
+}
--- Tree/Tree/Program.cs
+++ Tree/Tree/Program.cs
@@ -1,13 +1,58 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection.Metadata.Ecma335;
+
 Console.WriteLine("Hello, World!");
 
 
 public class BinaryNode
 {
+    private BinaryNode()
+    {
+
+    }
+
+    public BinaryNode(int value)
+    {
+        this.Value = value;
+    }
+
     public BinaryNode right;
     public BinaryNode left;
 
     public int Value;
+
+    public override bool Equals(object? obj)
+    {
+        if (obj == null || obj is not BinaryNode)
+            return false;
+
+        return this.Value == ((BinaryNode)obj).Value;
+    }
+
+    public class BinaryNodeBuilder
+    {
+        public BinaryNodeBuilder()
+        {
+            this.BinaryNode = new BinaryNode();
+        }
+
+        private BinaryNode BinaryNode;
+
+        public BinaryNodeBuilder AddLeft(BinaryNode leftBinaryNode)
+        {
+            this.BinaryNode.left = leftBinaryNode;
+            return this;
+        }
+        public BinaryNodeBuilder AddRight(BinaryNode rightBinaryNode)
+        {
+            this.BinaryNode.right = rightBinaryNode;
+            return this;
+        }
+        public BinaryNode Build()
+        {
+            return BinaryNode;
+        }
+    }
 }
 
 public class BinaryTree
@@ -26,7 +71,6 @@ public class BinaryTree
             Console.WriteLine(node.Value);
             InOrderTreeTraversal(node.right);
         }
-
         public void InorderTreeTraversal(BinaryNode node, List<int> binaryNodes)
         {
             if (node == null)
@@ -37,7 +81,6 @@ public class BinaryTree
             binaryNodes.Add(node.Value);
             InOrderTreeTraversal(node.right);
         }
-
         //Node Left Right
         public void PreOrderTreeTraversal(BinaryNode node)
         {
@@ -60,8 +103,18 @@ public class BinaryTree
             PostOrderTreeTraversal(node.right);
             Console.WriteLine(node.Value);
         }
-    }
 
+        public int HeightOfATree(BinaryNode binaryNode)
+        {
+            if (binaryNode == null)
+                return -1;
+
+            var leftHeight = HeightOfATree(binaryNode.left);
+            var rightHeight = HeightOfATree(binaryNode.right);
+
+            return 1 + Math.Max(leftHeight, rightHeight);
+        }
+    }
     public class IterativeBSTHelper
     {
         //Left To Right on each Level
@@ -79,8 +132,52 @@ public class BinaryTree
                 Console.WriteLine(currentNode.Value);
             }
         }
-
-        public int HeightOfTreeIterative(BinaryNode head)
+        public List<BinaryNode> LevelOrderTraversalLeftToRight(BinaryNode headNode)
+        {
+            if (headNode == null)
+            {
+                return new List<BinaryNode>();
+            }
+            Queue<BinaryNode> queue = new Queue<BinaryNode>();
+            queue.Enqueue(headNode);
+            queue.Enqueue(null);
+            var levelOrderTraversal = new List<BinaryNode>();
+            while (queue.Count > 1)
+            {
+                var currentNode = queue.Dequeue();
+                if (currentNode == null && queue.Count > 1)
+                {
+                    queue.Enqueue(null);
+                }
+                levelOrderTraversal.Add(currentNode);
+                if (currentNode.left != null)
+                    queue.Enqueue(currentNode.left);
+                if (currentNode.right != null)
+                    queue.Enqueue(currentNode.right);
+            }
+            return levelOrderTraversal;
+        }
+        public List<BinaryNode> LevelOrderTraversalRightToLeft(BinaryNode headNode)
+        {
+            if (headNode == null)
+            {
+                return new List<BinaryNode>();
+            }
+            Queue<BinaryNode> queue = new Queue<BinaryNode>();
+            queue.Enqueue(headNode);
+            var levelOrderTraversal = new List<BinaryNode>();
+            while (queue.Count > 0)
+            {
+                var currentNode = queue.Dequeue();
+                levelOrderTraversal.Add(currentNode.left);
+                if (currentNode.right != null)
+                    queue.Enqueue(currentNode.right);
+                if (currentNode.left != null)
+                    queue.Enqueue(currentNode.left);
+            }
+            return levelOrderTraversal;
+        }
+        public int HeightOfTreeIterativeWithNullNode(BinaryNode head) // N -> logN
         {
             if (head == null)
             {
@@ -88,29 +185,50 @@ public class BinaryTree
             }
             Queue<BinaryNode> queue = new Queue<BinaryNode>();
             queue.Enqueue(head);
-            BinaryNode dummyNode = null;
-            queue.Enqueue(dummyNode);
-            var height = 1;
-            while (queue.Count > 1)
+            queue.Enqueue(null);
+            var height = 0;
+            while (queue.Count > 0)
             {
                 var currentNode = queue.Dequeue();
                 if (currentNode == null)
                 {
                     height++;
-                    queue.Enqueue(dummyNode);
+                    if (queue.Count > 0)
+                        queue.Enqueue(null);
                     continue;
                 }
                 if (currentNode.left != null)
                     queue.Enqueue(currentNode.left);
                 if (currentNode.right != null)
                     queue.Enqueue(currentNode.right);
-
-                Console.WriteLine(currentNode.Value);
             }
-
             return height;
         }
-
+        public int HeightOfTreeIterative(BinaryNode head) // N -> logN
+        {
+            if (head == null)
+            {
+                return 0;
+            }
+            Queue<BinaryNode> queue = new Queue<BinaryNode>();
+            queue.Enqueue(head);
+            var height = 0;
+            while (queue.Count > 0)
+            {
+                var elementsInCurrentLevel = queue.Count;
+                for (var i = 0; i < elementsInCurrentLevel; i++)//Add all the next level element
+                {
+                    var currentNode = queue.Dequeue();
+                    if (currentNode.left != null)
+                        queue.Enqueue(currentNode.left);
+                    if (currentNode.right != null)
+                        queue.Enqueue(currentNode.right);
+                }
+                if (queue.Count > 1)//If next level has element add to height
+                    height++;
+            }
+            return height;
+        }
         public void InOrderTraversal(BinaryNode head)
         {
             Stack<BinaryNode> stack = new Stack<BinaryNode>();
@@ -127,6 +245,100 @@ public class BinaryTree
                 if (node.right != null)
                     currentNode = node.right;
             }
+        }
+        public List<BinaryNode> VerticalOrderTraversal(BinaryNode head)
+        {
+            if (head == null)
+                return new List<BinaryNode>();
+            Queue<(BinaryNode node, int height)> nodesWithHeight = new Queue<(BinaryNode node, int height)>();
+            nodesWithHeight.Enqueue((head, 0));
+
+            Dictionary<int, List<BinaryNode>> nodeAtEachHeight = new Dictionary<int, List<BinaryNode>>();
+
+            var maxHeight = 0;
+            var minHeight = 0;
+
+            while (nodesWithHeight.Count > 0)
+            {
+                var currentNode = nodesWithHeight.Dequeue();
+                maxHeight = Math.Max(maxHeight, currentNode.height);
+                minHeight = Math.Min(minHeight, currentNode.height);
+                if (nodeAtEachHeight.TryGetValue(currentNode.height, out var nodeList))
+                {
+                    nodeList.Add(currentNode.node);
+                }
+                else
+                {
+                    nodeAtEachHeight[currentNode.height] = new List<BinaryNode>() { currentNode.node };
+                }
+
+                if (currentNode.node.left != null)
+                {
+                    var leftNode = currentNode.node.left;
+                    nodesWithHeight.Enqueue((leftNode, currentNode.height - 1));
+                }
+                if (currentNode.node.right != null)
+                {
+                    var rightNode = currentNode.node.right;
+                    nodesWithHeight.Enqueue((rightNode, currentNode.height + 1));
+                }
+            }
+
+            var verticalOrderTraversal = new List<BinaryNode>();
+            for (int i = minHeight; i <= maxHeight; i++)
+            {
+                foreach (var node in nodeAtEachHeight[i])
+                {
+                    verticalOrderTraversal.Add(node);
+                }
+            }
+            return verticalOrderTraversal;
+        }
+        public void LeftView(BinaryNode headNode)
+        {
+            //First Element in level order with null identifier or
+            //prepare with height info as well and first element of each height
+        }
+        public void RightView()
+        {
+            //Last Element in level order(L->R) with null identifier or
+            //First Element in level order(R->L) with null identifier or
+            //prepare with height info as well and first element of each height
+        }
+        public void TopView()
+        {
+            //First item in each level in vertical order
+        }
+        public void BottomView()
+        {
+            //Last item in each level in vertical order
+        }
+
+        //Post Order and In Order
+        public BinaryNode CreateTreePostAndInOrder(Dictionary<int, int> inOrderNodePosition, List<int> postOrder, List<int> inOrder, int postStart, int postEnd, int inStart, int inEnd)
+        {
+            if (inStart > inEnd)
+                return null;
+
+            var currentNode = new BinaryNode(postOrder[postEnd]);
+            var inHeadposition = inOrderNodePosition[postOrder[postEnd]];
+            var noOfLeftElements = (inHeadposition - 1) - inStart + 1;
+            currentNode.left = CreateTreePostAndInOrder(inOrderNodePosition, postOrder, inOrder, postStart, postStart + noOfLeftElements, inStart, inHeadposition - 1);
+            currentNode.right = CreateTreePostAndInOrder(inOrderNodePosition, postOrder, inOrder, postStart + noOfLeftElements + 1, postEnd - 1, inHeadposition + 1, inEnd);
+            return currentNode;
+        }
+
+        public BinaryNode CreateTreePreAndInOrder(Dictionary<int, int> preNodePosition, List<int> preOrder, List<int> inOrder, int prestart, int preEnd, int inStart, int inEnd)
+        {
+            if (inStart > inEnd)
+                return null;
+
+            var currentNode = new BinaryNode(preOrder[prestart]);
+            var inHeadposition = preNodePosition[preOrder[prestart]];
+            int noOfItemOnPreLeft = (inHeadposition - 1) - inStart + 1;
+            currentNode.left = CreateTreePreAndInOrder(preNodePosition, preOrder, inOrder, prestart + 1, prestart + noOfItemOnPreLeft, inStart, inHeadposition - 1);
+            currentNode.right = CreateTreePreAndInOrder(preNodePosition, preOrder, inOrder, prestart + noOfItemOnPreLeft + 1, preEnd, inHeadposition + 1, inEnd);
+            return currentNode;
         }
     }
 }
